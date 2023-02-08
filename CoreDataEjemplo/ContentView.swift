464b5f6b53e426c7c75e8d620ba064b82425788a
Alvar Arias <alvararias@gmail.com>
2023-02-08 12:10:04 +0100

    Delete button

--- CoreDataEjemplo/ContentView.swift
+++ CoreDataEjemplo/ContentView.swift
@@ -11,29 +11,69 @@ struct ContentView: View {
     
     @Environment(\.managedObjectContext) var mylist
     
-    @FetchRequest(sortDescriptors: []) var products: FetchedResults<Producto>
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.nombre, order: .reverse)
+    ]
+    ) var products: FetchedResults<Producto>
     
     var body: some View {
-    
+        NavigationView {
         VStack {
-            List(products) { producto in
+            List {
+                
+            ForEach(products) { producto in
                 Text(producto.nombre ?? "Sin Nombre")
+                
+                    
+                    }
+                .onDelete(perform: removeLanguages)
                 }
-                
-            Button("Add") {
-                let Bateria = ["Bateria AAA", "Bateria AA", "Bateria 9V", "Bateria 12V"]
-                let Marca = ["Duracell", "RadioVac", "MyBrand", "NEC"]
-
-                let chosenBateria = Bateria.randomElement()!
-                let chosenMarca = Marca.randomElement()!
-
-                let product = Producto(context: mylist)
-                product.id = UUID()
-                product.nombre = "\(chosenBateria) \(chosenMarca)"
-                
-                try? mylist.save()
-            
             }
+        
+        .navigationTitle("Core DAta Items")
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            EditButton()
+        }
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Spacer()
+                Button("Add") {
+                    let Bateria = ["Bateria AAA", "Bateria AA", "Bateria 9V", "Bateria 12V"]
+                    let Marca = ["Duracell", "RadioVac", "MyBrand", "NEC"]
+
+                    let chosenBateria = Bateria.randomElement()!
+                    let chosenMarca = Marca.randomElement()!
+
+                    let product = Producto(context: mylist)
+                    product.id = UUID()
+                    product.nombre = "\(chosenBateria) \(chosenMarca)"
+                    
+                    try? mylist.save()
+                
+                    }
+                    
+                  Spacer()
+                    Button("Delete") {
+                        print("Delete")
+                        
+                    }
+                  Spacer()
+                    
+                }
+            }
+        }
+    }
+    
+    func removeLanguages(at offsets: IndexSet) {
+        for index in offsets {
+            let producto = products[index]
+            mylist.delete(producto)
+        }
+        do {
+            try mylist.save()
+        } catch {
+            print("error to save object")
         }
     }
 }
